05b9bb6cedcc76d593f51514935e152a04d8cc71
guillermo <h.uhm292@gmail.com>
2023-01-16 21:41:25 +0100

    realizado ejercicio uno

--- Readme.md
+++ Readme.md
@@ -1,1 +1,4 @@
 #javascript-ejercicio-parteuno
+
+## lista de ejercicios
+- Escribe un programa que escriba en la pantalla un texto de tipo <h1> que diga “Hello Javascript”
--- src/ex-1/app.js
+++ src/ex-1/app.js
@@ -1,1 +1,9 @@
-console.log("hello");
+/*Escribe un programa que escriba en la pantalla un texto de tipo <h1> que diga “Hello Javascript”  */
+
+// Crear contenedor para impresión del texto
+// Recuperar el contenedor en javascript
+// Insertar el texto
+
+
+
+ 
